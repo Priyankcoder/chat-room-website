e7104ed6f55f6efc52bc029cbd476de22b9fddf0
Priyankcoder <priyankrastogi14@gmail.com>
2020-07-11 13:03:12 +0530

    connect client to server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT||5000;
+const router = require("./router");
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket)=>{
+    console.log("New Connection");
+    socket.on('disconnect', ()=>{
+        console.log("User Left");
+    })
+});
+app.use(router);
+
+server.listen(PORT, ()=>console.log(`Server has started on port ${PORT}`));
